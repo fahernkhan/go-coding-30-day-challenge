69e5cafefdcf9a425a03f998f1c59d955d6ae945
fahernkhan <fathurrahmanhernanda123@gmail.com>
2025-03-18 13:08:20 +0700

    add array

--- day-3-array/twosum.go
+++ day-3-array/twosum.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func twoSum(nums []int, target int) []int {
 	for i, value := range nums {
 		for j := i + 1; j < len(nums); j++ {
@@ -11,5 +13,20 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
-// Anda bisa menambahkan fungsi main untuk mencoba twoSum di sini,
-// tapi untuk testing, main_test.go sudah cukup.
+func main() {
+	testCases := []struct {
+		nums   []int
+		target int
+		expect []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+		{[]int{3, 3}, 6, []int{0, 1}},
+		{[]int{1, 2, 3, 4, 5}, 10, nil},
+	}
+
+	for _, testCase := range testCases {
+		result := twoSum(testCase.nums, testCase.target)
+		fmt.Printf("nums: %v, target: %d, expected: %v, got: %v\n", testCase.nums, testCase.target, testCase.expect, result)
+	}
+}
