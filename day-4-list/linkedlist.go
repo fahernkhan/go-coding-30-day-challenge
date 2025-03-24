f4dfe7f12d1268e4c72dfae2bba846ad258d976c
fahernkhan <fathurrahmanhernanda123@gmail.com>
2025-03-24 22:34:29 +0700

    add linkedlist head

--- day-4-list/linkedlist.go
+++ day-4-list/linkedlist.go
@@ -14,13 +14,16 @@ type LinkedList struct {
 }
 
 // AddToHead method of LinkedList class
-func (LinkedList *LinkedList) AddToHead(property int) {
-	var node = Node{}
+func (linkedList *LinkedList) AddToHead(property int) {
+	var node = &Node{}
 	node.property = property
-	if node.nextNode != nil {
-		node.nextNode = LinkedList.headNode
+	node.nextNode = nil
+
+	if linkedList.headNode != nil {
+		node.nextNode = linkedList.headNode
 	}
-	LinkedList.headNode = &node
+
+	linkedList.headNode = node
 }
 
 // ItarateList method iterates over LinkedList
@@ -31,10 +34,78 @@ func (linkedList *LinkedList) IterateList() {
 	}
 }
 
+// Last method returns the last Node
+func (linkedList *LinkedList) LastNode() *Node {
+	var node *Node
+	var lastNode *Node
+	for node = linkedList.headNode; node != nil; node = node.nextNode {
+		if node.nextNode == nil {
+			lastNode = node
+		}
+	}
+	return lastNode
+}
+
+// AddToEnd method adds the node with property to the end
+func (linkedList *LinkedList) AddToEnd(property int) {
+	var node = &Node{}
+	node.property = property
+	node.nextNode = nil
+	var lastNode *Node
+	lastNode = linkedList.LastNode()
+	// misal last node dah ke-isi
+	if lastNode != nil {
+		lastNode.nextNode = node
+	}
+}
+
+// NodeWithValue method returns Node given parameter property
+func (linkedlist LinkedList) NodeWithValue(property int) *Node {
+	var node *Node
+	var nodeWith *Node
+	for node = linkedlist.headNode; node != nil; node = node.nextNode {
+		if node.property == property {
+			nodeWith = node
+			break
+		}
+	}
+	return nodeWith
+}
+
+// AddAfter method adds a node with nodeProperty after node with property
+func (linkedList *LinkedList) AddAfter(nodeProperty int, property int) {
+	var node = &Node{}
+	node.property = property
+	node.nextNode = nil
+	// Mencari node dengan nilai nodeProperty
+	var nodeWith *Node
+	nodeWith = linkedList.NodeWithValue(nodeProperty)
+
+	// Jika node ditemukan
+	if nodeWith != nil {
+		// Langkah 1: Hubungkan node baru ke node setelah nodeWith
+		node.nextNode = nodeWith.nextNode
+		// Langkah 2: Hubungkan nodeWith ke node baru
+		nodeWith.nextNode = node
+	}
+
+}
+
 func main() {
 	var linkedList LinkedList
+
 	linkedList = LinkedList{}
+
 	linkedList.AddToHead(1)
 	linkedList.AddToHead(3)
-	fmt.Println(linkedList.headNode.property)
+	linkedList.AddToHead(6)
+	// fmt.Println(linkedList.headNode.property)
+	linkedList.AddToEnd(5)
+	linkedList.AddAfter(1, 7)
+	linkedList.AddAfter(7, 24)
+	linkedList.AddToEnd(100)
+
+	// Expected list is [6, 3, 1, 7, 24, 5, 100]
+	linkedList.IterateList()
+
 }
---
+++ day-4-list/loop.go
@@ -0,0 +1,13 @@
+package main
+
+// loop concept
+// for init; condition; update {
+// 	// body
+// }
+// init → node = linkedList.headNode (Mulai dari head node)
+
+// condition → node != nil (Berhenti jika mencapai akhir linked list)
+
+// update → node = node.nextNode (Pindah ke node berikutnya)
+
+// dari base case kecil ke besar dan dari belakang atau sepele ke depan atau ke penting (low to high)
