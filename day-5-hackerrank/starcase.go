1346e3988dd4d79515e47db3dd6b3eb29a4992c2
fahernkhan <fathurrahmanhernanda123@gmail.com>
2025-03-29 19:18:57 +0700

    add algorithm

---
+++ day-5-hackerrank/starcase.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func staircase(n int32) {
+	// Write your code here
+	n2 := n - 1
+	star := 1
+	for i := 0; i < int(n); i++ {
+		for j := n2; j > 0; j-- {
+			fmt.Print(" ")
+		}
+
+		for k := 0; k < star; k++ {
+			fmt.Print("#")
+		}
+
+		star++
+		n2--
+		fmt.Print("\n")
+	}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	staircase(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
