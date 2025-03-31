925ecd32a9b10c582718ce933fbc68d74aed94f3
fahernkhan <fathurrahmanhernanda123@gmail.com>
2025-03-31 22:20:47 +0700

    add data structure

---
+++ day-2-fundamental/search_algorithms.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// printIndex prints the index of the searched number
+func printIndex(numbers []int, target int) {
+	index := sort.SearchInts(numbers, target)
+	fmt.Printf("Angka %d ditemukan pada indeks %d\n", target, index)
+}
+func main() {
+	// Search for the index of an element in a sorted slice of integers using binary search
+	// Data slice yang sudah terurut
+	numbers := []int{1, 2, 3, 4, 5, 6}
+
+	// Cari angka tertentu dalam slice
+	target := 4
+
+	// Cetak hasil pencarian
+	printIndex(numbers, target)
+
+}
